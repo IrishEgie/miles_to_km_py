0cce4f3063aa9c031931c0523339eb6908af7c60
ejarao <irishegie.arao@ssu.edu.ph>
2024-09-16 17:29:39 +0800

    update: finished the tkinter GUI miles to km program

---
+++ main.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+
+FONT = ("Courier", 10)
+window = tk.Tk()
+window.title("Miles to Km")
+# window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+window.eval('tk::PlaceWindow . center')
+
+def button_click():
+    # Check if the input is a valid number
+    user_input = in_field.get()
+    
+    try:
+        miles = float(user_input)  # Convert to float for flexibility (in case of decimal values)
+        km = miles * 1.60934  # Convert miles to kilometers
+        calculated_km.config(text=f"{km:.2f}")  # Update label with km value
+        notif_label.config(text="")  # Clear any previous error message
+    except ValueError:
+        notif_label.config(text="Please input numbers only",fg="reg")  # Display error message
+#Label 
+label = tk.Label(text="Is equal to", font=FONT)
+calculated_km = tk.Label(text="0", font=FONT)
+unit_miles = tk.Label(text="miles", font=FONT,padx=20)
+unit_km = tk.Label(text="km", font=FONT,padx=20)
+notif_label = tk.Label(text="", font=FONT)
+#Button
+button = tk.Button(text="Calculate",command=button_click, padx=5, pady=5,font=FONT)
+
+#Entry field
+in_field = tk.Entry()
+in_field.config(width=25)
+
+in_field.grid(column=1, row=0)
+unit_miles.grid(column=2, row=0)
+label.grid(column=0, row=1)
+calculated_km.grid(column=1, row=1)
+unit_km.grid(column=2, row=1)
+button.grid(column=1, row=2)
+notif_label.grid(column=1, row=3)  # Notification label layout
+
+
+window.mainloop()
--- practice.py
+++ practice.py
@@ -4,14 +4,13 @@ FONT = ("Courier", 14, "bold")
 window = tkinter.Tk()
 window.title("Miles to Km")
 window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
 
 # def add(*args):
 #     add_list = [n for n in args]
 #     return sum(add_list)
 
 # print(add(1,2,3,4,5,4,6,7,5))
-
-
 
 def button_click():
     user_en = in_field.get()
@@ -22,13 +21,16 @@ def button_click():
 label = tkinter.Label(text="This is a label", font=FONT)
 #Button
 button = tkinter.Button(text="Button",command=button_click)
+button2 = tkinter.Button(text="New Button",command=button_click)
 #Entry
 in_field = tkinter.Entry()
 
 
-label.pack()
-in_field.pack() 
-button.pack()
+label.grid(column=0, row=0)
+button.grid(column=1, row=1)
+button2.grid(column=2, row=0)
+in_field.grid(column=3, row=2)
+
 
 window.mainloop()
 
